69a54001aa831f9b44e38d5a5b32a644eaa822ed
ian <iancazalillacerro@gmail.com>
2026-02-26 19:28:33 +0100

    listo para arrglar el juego

--- frontend-client/src/components/Auth/RegisterForm.jsx
+++ frontend-client/src/components/Auth/RegisterForm.jsx
@@ -10,6 +10,11 @@ export const RegisterForm = () => {
     e.preventDefault();
     setErrors({}); // Limpiar errores previos
     try {
+        if (formData.password !== e.target.password_confirmation.value) {
+            setErrors({ password_confirmation: ["Las contraseñas no coinciden."] });
+            return;
+        }
+        
         await api.post('/register', formData);
         alert("¡Registro éxito! Ahora loguéate.");
         redirect('/login'); // Redirigir al login tras registrarse
@@ -28,22 +33,27 @@ export const RegisterForm = () => {
             <input 
                 type="text" placeholder="Username" 
                 onChange={(e) => setFormData({...formData, username: e.target.value})} 
-                className="p-2 border"
+                className="p-2 border" required
             />
             {errors.username && <span className="text-red-500">{errors.username[0]}</span>}
             
             <input 
                 type="email" placeholder="Email" 
                 onChange={(e) => setFormData({...formData, email: e.target.value})} 
-                className="p-2 border"
+                className="p-2 border" required
             />
+            {errors.email && <span className="text-red-500">{errors.email[0]}</span>}
             
             <input 
                 type="password" placeholder="Contraseña (mín. 6 caracteres)" 
                 onChange={(e) => setFormData({...formData, password: e.target.value})} 
-                className="p-2 border"
+                className="p-2 border" required
             />
+            {errors.password && <span className="text-red-500">{errors.password[0]}</span>}
             
+            <input type="password" placeholder="Confirmar Contraseña" className="p-2 border"  required />
+            {errors.password_confirmation && <span className="text-red-500">{errors.password_confirmation[0]}</span>}
+
             <button type="submit" className="bg-blue-500 text-white p-2 rounded">Registrarse</button>
         </form>
     );
